f03fbcf278e2c14f2c6d95cbef93a0fa45a9f146
魏江龙 <david.wei@eclicktech.com.cn>
2023-02-24 15:58:30 +0800

    fix:package main

--- main/main.go
+++ main/main.go
@@ -1,44 +1,42 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"
-	"log"
-	"os"
 	"soketproxy/proxy"
 	_ "soketproxy/proxy"
 )
 
 func main() {
-	app := &cli.App{
-		Name: "proxy",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "userName",
-				Aliases:  []string{"u"},
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "password",
-				Aliases:  []string{"p"},
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "server",
-				Aliases:  []string{"s"},
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "port",
-				Aliases:  []string{"po"},
-				Required: true,
-			},
-		},
-		Action: func(ctx *cli.Context) error {
-			return proxy.Run(ctx.String("userName"), ctx.String("password"), ctx.String("server"), ctx.Int("port"))
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	//app := &cli.App{
+	//	Name: "proxy",
+	//	Flags: []cli.Flag{
+	//		&cli.StringFlag{
+	//			Name:     "userName",
+	//			Aliases:  []string{"u"},
+	//			Required: true,
+	//		},
+	//		&cli.StringFlag{
+	//			Name:     "password",
+	//			Aliases:  []string{"p"},
+	//			Required: true,
+	//		},
+	//		&cli.StringFlag{
+	//			Name:     "server",
+	//			Aliases:  []string{"s"},
+	//			Required: true,
+	//		},
+	//		&cli.StringFlag{
+	//			Name:     "port",
+	//			Aliases:  []string{"po"},
+	//			Required: true,
+	//		},
+	//	},
+	//	Action: func(ctx *cli.Context) error {
+	//		return proxy.Run(ctx.String("userName"), ctx.String("password"), ctx.String("server"), ctx.Int("port"))
+	//	},
+	//}
+	//
+	//if err := app.Run(os.Args); err != nil {
+	//	log.Fatal(err)
+	//}
+	proxy.Run1()
 }
--- proxy/clash.go
+++ proxy/clash.go
@@ -50,6 +50,42 @@ func Run(userName string, password string, server string, port int) (error error
 	return err
 }
 
+func Run1() {
+	in := make(chan constant.ConnContext, 100)
+	defer close(in)
+
+	l, err := mixed.New("0.0.0.0:20001", in)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	println("listen at:", l.Address())
+	socksOption := &outbound.Socks5Option{
+		Name:     "ipProxy",
+		Server:   "proxy.ipidea.io",
+		Port:     2333,
+		UserName: "uat_team-zone-custom-region-us-session-uat2023vcad-sessTime-30",
+		Password: "314f71aaa600567a315b405626102ed4",
+	}
+	direct := outbound.NewSocks5(*socksOption)
+	//direct := outbound.NewDirect()
+	//auth.SetAuthenticator(auth2.NewAuthenticator([]auth2.AuthUser{{User: "testAdmin", Pass: "904028"}}))
+	//auth.SetAuthenticator(simpleAuth{})
+	for c := range in {
+		conn := c
+		metadata := conn.Metadata()
+		fmt.Printf("request incoming from %s to %s\n", metadata.SourceAddress(), metadata.RemoteAddress())
+		go func() {
+			remote, err := direct.DialContext(context.Background(), metadata)
+			if err != nil {
+				fmt.Printf("dial error: %s\n", err.Error())
+				return
+			}
+			relay(remote, conn.Conn())
+		}()
+	}
+}
+
 type simpleAuth struct {
 }
 
